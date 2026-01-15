edd49e5a40af8fcd47d48d46e000669fc2baff13
Prince Lee <prince_lee.nna85@ymail.com>
2026-01-15 17:28:21 +0100

    fix register issue

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI2, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
